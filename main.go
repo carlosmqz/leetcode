0aec7bba9bf3f8767e8f24d446ad7458ffbda3c4
carlosmqz <carlosmqz@gmail.com>
2022-08-17 18:51:01 -0500

    jump cloud solution

--- main.go
+++ main.go
@@ -16,5 +16,9 @@ func main() {
 	//fmt.Println(timeConv)
 	//fmt.Println(solutions.CircularArrayRotation([]int32{3, 4, 5}, 2, []int32{1, 2}))
 	//fmt.Println(solutions.RepeatedString("afcfffaged", 962645758455))
-	fmt.Println(solutions.RepeatedString("aba", 10))
+	//fmt.Println(solutions.RepeatedString("aba", 10))
+	fmt.Println(solutions.JumpingOnClouds([]int32{0, 0, 0, 1, 0, 0}))    //3
+	fmt.Println(solutions.JumpingOnClouds([]int32{0, 0, 1, 0, 0, 1, 0})) //4
+	fmt.Println(solutions.JumpingOnClouds([]int32{0, 0, 0, 0, 0, 1, 0})) //3
+
 }
---
+++ solutions/jumpingClouds.go
@@ -0,0 +1,23 @@
+package solutions
+
+func JumpingOnClouds(c []int32) int32 {
+	// Write your code here
+	//var lastone int32
+	//0, 0, 0, 1, 0, 0
+	var jumpCounter, l, i int32
+	l = int32(len(c))
+	for i = 0; i < l-1; {
+		if i+2 < l && c[i+2] == 0 {
+			jumpCounter++
+			i += 2
+			continue
+		}
+		if i+1 < l && c[i] == 0 {
+			jumpCounter++
+			i += 1
+		}
+
+	}
+
+	return jumpCounter
+}
